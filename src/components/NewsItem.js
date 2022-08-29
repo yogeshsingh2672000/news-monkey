54aab8bd345acf14c7eeb83bd36497ee67bb4f0c
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-08-29 21:54:34 +0530

    added author and date

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,37 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
         <div className="card">
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <span
+            className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+            style={{ zIndex: "1" }}
+          >
+            {source}
+          </span>
+          <img
+            src={
+              !imageUrl
+                ? "https://images.pexels.com/photos/1928151/pexels-photo-1928151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{!title ? "Unknown Title" : title}</h5>
+            <p className="card-text">
+              {!description ? "Click on Readmore" : description}
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toUTCString()}
+              </small>
+            </p>
             <a
               rel="noreferrer"
               href={newsUrl}
